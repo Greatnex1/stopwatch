b06704fb7e23fbdecb5783491bd2494f38f33f56
Nouah <noahgreatakoni@gmail.com>
2022-10-06 14:25:56 +0100

    stopwatch code

--- javasript.js
+++ javasript.js
@@ -10,7 +10,38 @@ window.onload= function(){
 
     buttonStart.onclick = function(){
         clearInterval(interval);
-        interval = setInterval(start)
+        interval = setInterval(startTimer, 10)
     }
+    stopButton.onclick = function(){
+        clearInterval(interval)
+    }
+resetButton.onclick = function(){
+    clearInterval(interval)
+    tens = "00"
+    seconds = "00";
+    appendTens.innerHTML = tens;
+    appendSeconds.innerHTML = seconds;
 
+}
+function startTimer (){
+    tens++;
+    if(tens<= 9){
+        appendTens.innerHTML = "0" + tens;
+
+    }
+    if(tens > 9){
+        appendTens.innerHTML = tens;
+    }
+    if (tens  > 99){
+        console.log("seconds");
+        seconds++;
+        appendSeconds.innerHTML = "0" + seconds
+
+        tens = 0;
+        appendTens.innerHTML = "0" + 0;
+    }
+    if(seconds > 9){
+        appendSeconds.innerHTML = seconds
+    }
+}
 }
